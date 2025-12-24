510596d3b3b02f111bd935278516a00c6b693c05
Mohamed Raafat <t-malgharib@microsoft.com>
2025-12-24 23:17:58 +0200

    Refactor appsettings.json for CosmosDb database name; add configuration classes for CosmosDb, GitHub, JWT, and LeetCodeApi settings

---
+++ LevelUpDev.Infrastructure/Settings/GitHubSettings.cs
@@ -0,0 +1,7 @@
+namespace LevelUpDev.Infrastructure.Settings;
+
+public class GitHubSettings
+{
+    public required string ClientId { get; init; }
+    public required string ClientSecret { get; init; }
+}
