21b9c8e47ef5831364b645c6166cba3fd4bf8418
Mohamed Raafat <t-malgharib@microsoft.com>
2025-12-25 01:13:47 +0200

    chore: remove deprecated files
    - Remove WeatherForecastController (template code)
    - Remove old Settings classes (replaced by Configuration)

--- LevelUpDev.Infrastructure/Settings/GitHubSettings.cs
+++
@@ -1,7 +0,0 @@
-namespace LevelUpDev.Infrastructure.Settings;
-
-public class GitHubSettings
-{
-    public required string ClientId { get; init; }
-    public required string ClientSecret { get; init; }
-}
--- LevelUpDev.Infrastructure/Settings/JwtSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace LevelUpDev.Infrastructure.Settings;
-
-public class JwtSettings
-{
-    public required string Issuer { get; init; }
-    public required string Audience { get; init; }
-    public required string SecretKey { get; init; }
-    public required int ExpirationMinutes { get; init; } = 10080; // 7 days
-}
